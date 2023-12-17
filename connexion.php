edd60addecc71d7644f15137a61dbd895b905987
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-12-17 19:31:03 +0100

    modify connexion

--- connexion.php
+++ connexion.php
@@ -1,22 +1,23 @@
 <?php
 
-class Connexion{
-    private $servername = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $dbname = 'you_article';
-    private $conn; 
-
-    public function __construct() {
+class Connexion {
+    private string $servername = 'localhost';
+    private string $username = 'root';
+    private string $password = '';
+    private string $dbname = 'articles';
+    
+    protected function connexion() {
         try {
-            $this->conn = new PDO('mysql:host='.$this->servername.';dbname='.$this->dbname, $this->username, $this->password);
-            echo "Connected to $dbname at $host successfully.";
-        } 
-        catch(PDOException $e) {
-            die('Erreur : '.$e->getMessage());
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password );
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
         }
-        
+        return $conn;
     }
+        
 }
 
-?>
+
